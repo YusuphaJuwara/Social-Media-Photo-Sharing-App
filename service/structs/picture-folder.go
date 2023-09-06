cd6a858d78eb18444c2dff304947daf29f02a27f
YusuphaJuwara <yusuphajuwara@gmail.com>
2023-09-06 05:22:48 +0200

    Solve the docker certificate error when downloading the default profile picture

--- service/structs/picture-folder.go
+++ service/structs/picture-folder.go
@@ -1,6 +1,7 @@
 package structs
 
 import (
+	"crypto/tls" // Import the crypto/tls package for configuring TLS
 	"fmt"
 	"io"
 	"net/http"
@@ -13,37 +14,34 @@ const URL = "https://w7.pngwing.com/pngs/269/897/png-transparent-family-cosmetic
 const FileName = "ffffffff-ffff-ffff-ffff-ffffffffffff.png"
 
 func PicFolderDownloadAndSavePhoto() error {
+	// Configure a transport that disables certificate verification to bypass the docker run error.
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	// Create an HTTP client with the custom transport
+	client := &http.Client{Transport: tr}
 
 	err := os.MkdirAll(PicFolder, os.ModeDir|os.ModePerm)
 	if err != nil {
-
-		// If path is already a directory, MkdirAll does nothing and returns nil.
-		// if !os.IsExist(err) {
-		// 	logger.WithError(err).Error("error regarding the pictures folder")
-		// 	return fmt.Errorf("error creating/checking pictures folder: %w", err)
-		// }
-
 		return fmt.Errorf("error creating pictures folder: \n\tPicFolder: %s \n\tError: %w", PicFolder, err)
 	}
 
-	// Create the full file path
 	fullFilePath := filepath.Join(PicFolder, FileName)
 
-	// Make an HTTP GET request to the URL
-	resp, err := http.Get(URL)
+	// Make an HTTP GET request to the URL using the custom client
+	resp, err := client.Get(URL)
 	if err != nil {
 		return fmt.Errorf("error downloading default profile photo at App startup: \n\tURL: %s \n\tError: %w", URL, err)
 	}
 	defer resp.Body.Close()
 
-	// Open the file for writing
 	file, err := os.Create(fullFilePath)
 	if err != nil {
 		return fmt.Errorf("error creating the file path: \n\tFilename: %s \n\tError: %w", FileName, err)
 	}
 	defer file.Close()
 
-	// Copy the response body to the file
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
 		return fmt.Errorf("error with io.Copy(...): \n\tError: %w", err)
